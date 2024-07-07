f4fe1a3c9eab74ac72e40f278ddebf4ef482f2f9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-07-07 23:00:10 +0900

    adjust types

--- src/commonMain/kotlin/me/geso/kdary/DoubleArrayUnit.kt
+++ src/commonMain/kotlin/me/geso/kdary/DoubleArrayUnit.kt
@@ -6,19 +6,13 @@ value class DoubleArrayUnit(
     val unit: IdType = 0u,
 ) {
     // hasLeaf() はユニットからすぐに派生したリーフユニットかどうかを返します（trueの場合）またはそうでない場合（false）。
-    fun hasLeaf(): Boolean {
-        return ((unit.toInt() shr 8) and 1) == 1
-    }
+    fun hasLeaf(): Boolean = ((unit.toInt() shr 8) and 1) == 1
 
     // value() はユニットに格納されている値を返します。したがって、value() はユニットがリーフユニットである場合にのみ利用可能です。
-    fun value(): ValueType {
-        return (unit and ((1u shl 31) - 1u)).toInt()
-    }
+    fun value(): ValueType = (unit and ((1u shl 31) - 1u)).toValueType()
 
     // label() はユニットに関連付けられたラベルを返します。リーフユニットは常に無効なラベルを返します。この機能のために、リーフユニットのlabel() はMSBが1の<id_type>を返します。
-    fun label(): IdType {
-        return unit and ((1u shl 31) or 0xFFu)
-    }
+    fun label(): IdType = unit and ((1u shl 31) or 0xFFu)
 
     // offset() はユニットから派生したユニットへのオフセットを返します。
     fun offset(): IdType {
