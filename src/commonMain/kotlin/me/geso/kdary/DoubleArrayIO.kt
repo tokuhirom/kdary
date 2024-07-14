8ff5de23c7ec177689b72884d4145f9161b6b2a4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-07-14 23:13:05 +0900

    simplify

--- src/commonMain/kotlin/me/geso/kdary/DoubleArrayIO.kt
+++ src/commonMain/kotlin/me/geso/kdary/DoubleArrayIO.kt
@@ -25,3 +25,33 @@ expect fun saveDoubleArray(
     doubleArray: DoubleArray,
     fileName: String,
 )
+
+/**
+ * Reads an unsigned 32-bit integer in little-endian format from the given source.
+ *
+ * @param source The BufferedSource to read from.
+ * @return The unsigned 32-bit integer read from the source.
+ */
+internal fun readUIntLe(source: okio.BufferedSource): UInt {
+    val byte1 = source.readByte().toUInt() and 0xFFU
+    val byte2 = source.readByte().toUInt() and 0xFFU
+    val byte3 = source.readByte().toUInt() and 0xFFU
+    val byte4 = source.readByte().toUInt() and 0xFFU
+    return (byte1 or (byte2 shl 8) or (byte3 shl 16) or (byte4 shl 24))
+}
+
+/**
+ * Writes an unsigned 32-bit integer in little-endian format to the given sink.
+ *
+ * @param sink The BufferedSink to write to.
+ * @param value The unsigned 32-bit integer to write.
+ */
+internal fun writeUIntLe(
+    sink: okio.BufferedSink,
+    value: UInt,
+) {
+    sink.writeByte((value and 0xFFU).toInt())
+    sink.writeByte(((value shr 8) and 0xFFU).toInt())
+    sink.writeByte(((value shr 16) and 0xFFU).toInt())
+    sink.writeByte(((value shr 24) and 0xFFU).toInt())
+}
--- src/jvmMain/kotlin/me/geso/kdary/DoubleArrayIO.jvm.kt
+++ src/jvmMain/kotlin/me/geso/kdary/DoubleArrayIO.jvm.kt
@@ -10,36 +10,6 @@ import kotlin.io.path.exists
 import kotlin.io.path.fileSize
 
 /**
- * Reads an unsigned 32-bit integer in little-endian format from the given source.
- *
- * @param source The BufferedSource to read from.
- * @return The unsigned 32-bit integer read from the source.
- */
-private fun readUIntLe(source: okio.BufferedSource): UInt {
-    val byte1 = source.readByte().toUInt() and 0xFFU
-    val byte2 = source.readByte().toUInt() and 0xFFU
-    val byte3 = source.readByte().toUInt() and 0xFFU
-    val byte4 = source.readByte().toUInt() and 0xFFU
-    return (byte1 or (byte2 shl 8) or (byte3 shl 16) or (byte4 shl 24))
-}
-
-/**
- * Writes an unsigned 32-bit integer in little-endian format to the given sink.
- *
- * @param sink The BufferedSink to write to.
- * @param value The unsigned 32-bit integer to write.
- */
-private fun writeUIntLe(
-    sink: okio.BufferedSink,
-    value: UInt,
-) {
-    sink.writeByte((value and 0xFFU).toInt())
-    sink.writeByte(((value shr 8) and 0xFFU).toInt())
-    sink.writeByte(((value shr 16) and 0xFFU).toInt())
-    sink.writeByte(((value shr 24) and 0xFFU).toInt())
-}
-
-/**
  * Saves the double array into the specified file.
  *
  * @param fileName The name of the file to save to.
